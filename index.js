d25867b8d6aeb88300738235f905bc3e221a5f9d
raglandcodes <raglandasir@gmail.com>
2020-03-18 19:45:48 +0530

    remove unnecessary files and code

--- index.js
+++ index.js
@@ -1,41 +1,13 @@
 // --- constants / globals---
 const paper = document.querySelector('#paper');
-const alphabets = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const paperPadding = 15;
 
 let selectionStart = 0;
 let selectionEnd = 0;
-console.log(`${alphabets.length} <== alphabets.length`);
 let mouseClicked = false;
 
 let styles = [];
-// --- sizer ---
-// const sizer = document.querySelector('#sizer');
-// let sizes = {};
-
-// alphabets.split('').forEach(alphabet => {
-//   sizer.innerText = alphabet;
-//   sizes[alphabet] = sizer.offsetWidth;
-// });
-
-// let size = sizer.offsetWidth;
-// //console.log(`${size} <== size`);
-// paper.addEventListener('keydown', e => {
-//   return;
-// });
 
 // --- DOM ---
-paper.addEventListener('mousedown', e => {
-  mouseClicked = true;
-  //console.dir(e);
-  console.log(`${e.clientX} <== e.clientX`);
-  console.log(`${e.clientY} <== e.clientY`);
-});
-
-// paper.addEventListener('mousemove', () => {
-//   if (mouseClicked) {
-//   }
-// });
 
 paper.addEventListener('mouseup', e => {
   mouseClicked = false;
