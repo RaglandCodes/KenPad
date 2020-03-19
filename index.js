eaedf79142f362d8045156e39a9bf72741ccdcaa
raglandcodes <raglandasir@gmail.com>
2020-03-19 20:54:13 +0530

    new method

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// --- dependencies ---
+
+//const jsdom = require('jsdom');
 // --- constants / globals---
 const paper = document.querySelector('#paper');
 
@@ -5,12 +8,98 @@ let selectionStart = 0;
 let selectionEnd = 0;
 let mouseClicked = false;
 
+plainText = '';
 let styles = [];
 
 // --- DOM ---
 
-paper.addEventListener('mouseup', e => {
-  mouseClicked = false;
+function generateStyledHTML() {
+  let html = '';
+  let plainText = paper.innerText;
+  for (let i = 0; i < plainText.length; i++) {
+    for (let j = 0; j < styles.length; j++) {
+      if (styles[j][0] === i) {
+        html += styles[j][1];
+      }
+    }
+    html += plainText[i];
+  }
+
+  console.log(`${JSON.stringify(html, null, 2)} <== final html in gsh`);
+  return html;
+}
+
+function addNewStyle(style, startIndex, endIndex) {
+  if (style === 'bold') {
+    console.log('BB');
+
+    styles.push([selectionStart, '<b>'], [selectionEnd, '</b>']);
+  } else if (style === 'italics') {
+    styles.push([selectionStart, '<i>'], [selectionEnd, '</i>']);
+  } else if (style === 'underline') {
+    styles.push([selectionStart, '<u>'], [selectionEnd, '</u>']);
+  }
+}
+
+paper.addEventListener('keydown', e => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    console.log('EE');
+  }
+  console.log(`${e.key} <== e.key`);
+  console.log(`${e.key === ' '} <== e.key === " "`);
+  console.log(`${e.key === 'Enter'} <== e.key === "Enter"`);
+  console.log(`${paper.innerHTML} <== paper.innerHTML`);
+});
+
+function setStyleAndTextFromHTML() {
+  html = paper.innerHTML;
+  html = html.trim();
+  console.log(`${html} <== html`);
+  let p = '';
+  //let s = [];
+  styles = [];
+  let extraCounter = 0;
+
+  let htmlLength = html.length;
+  let currentIndex = -1;
+  // console.log(`${htmlLength} <== htmlLength`);
+  while (currentIndex < htmlLength) {
+    currentIndex += 1;
+    // console.log(`${currentIndex} <== currentIndex ==> ${html[currentIndex]}`);
+    if (html[currentIndex] === '<') {
+      if (html[currentIndex + 1] === 'b') {
+        styles.push([currentIndex - extraCounter, '<b>']);
+        currentIndex += 2;
+        extraCounter += 3;
+      } else if (html[currentIndex + 1] === 'i') {
+        styles.push([currentIndex - extraCounter, '<i>']);
+        currentIndex += 2;
+        extraCounter += 3;
+      } else if (html[currentIndex + 1] === 'u') {
+        styles.push([currentIndex - extraCounter, '<u>']);
+        currentIndex += 2;
+        extraCounter += 3;
+      } else if (html[currentIndex + 1] === '/') {
+        if (html[currentIndex + 2] === 'b') {
+          styles.push([currentIndex - extraCounter, '</b>']);
+        } else if (html[currentIndex + 2] === 'i') {
+          styles.push([currentIndex - extraCounter, '</i>']);
+        } else if (html[currentIndex + 2] === 'u') {
+          styles.push([currentIndex - extraCounter, '</u>']);
+        }
+        currentIndex += 3;
+        extraCounter += 4;
+      }
+      continue;
+    }
+  }
+
+  // styles = s;
+}
+
+function handleStyleClick(style) {
+  console.log(`${style} <== style`);
 
   let selection = window.getSelection().getRangeAt(0);
   let selectionLength = selection.endOffset - selection.startOffset;
@@ -22,46 +111,21 @@ paper.addEventListener('mouseup', e => {
   selectionEnd = preCaretRange.toString().length;
   selectionStart = selectionEnd - selectionLength;
 
-  //   console.log(`${selectionLength} <== selectionLength`);
-  //   console.log(`${selectionStart} <== selectionStart`);
-  //   console.log(`${selectionEnd} <== selectionEnd`);
-
-  //   console.log(`${paper.innerHTML} <== paper.innerHTML`);
-});
-
-function handleStyleClick(style) {
-  console.log(`${style} <== style`);
-
   if (selectionStart === selectionEnd) {
     console.log('Not doing anything');
     return;
   }
 
-  if (style === 'bold') {
-    styles.push([selectionStart, '<b>'], [selectionEnd, '</b>']);
-  } else if (style === 'italics') {
-    styles.push([selectionStart, '<i>'], [selectionEnd, '</i>']);
-  } else if (style === 'underline') {
-    styles.push([selectionStart, '<u>'], [selectionEnd, '</u>']);
-  }
+  setStyleAndTextFromHTML();
+  // console.log(`${JSON.stringify(styles, null, 2)} <== styles btw`);
+  addNewStyle(style, selectionStart, selectionEnd);
 
   // reset the selection values
   selectionStart = 0;
   selectionEnd = 0;
-  console.log(`${JSON.stringify(styles, null, 2)} <== styles`);
 
-  let styledText = '';
-  let oldText = paper.innerText;
-  for (let i = 0; i < oldText.length; i++) {
-    for (let j = 0; j < styles.length; j++) {
-      if (styles[j][0] === i) {
-        styledText += styles[j][1];
-      }
-    }
-    styledText += oldText[i];
-  }
-
-  paper.innerHTML = styledText;
+  // console.log(`${JSON.stringify(styles, null, 2)} <== styles before geeratinh new HTML`);
+  paper.innerHTML = generateStyledHTML();
 }
 
 function toggleTheme() {
